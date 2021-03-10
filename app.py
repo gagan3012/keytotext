e9b8f5e01bff28f54ae11484540d28bc07d21406
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-03-10 18:38:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ import streamlit as st
 from transformers import AutoTokenizer, AutoModelWithLMHead
 import re
 
-
 st.set_page_config(
     page_title="Text Generation Using Keywords",
     layout="wide",
     initial_sidebar_state="expanded", )
 
-@st.cache(suppress_st_warning=True,ttl=1000)
-def generate(keywords,temp,top_p):
+
+@st.cache(suppress_st_warning=True, ttl=1000)
+def generate(keywords, temp, top_p):
     tokenizer = AutoTokenizer.from_pretrained("gagan3012/keytotext-small")
     model = AutoModelWithLMHead.from_pretrained("gagan3012/keytotext-small")
     text = str(keywords)
@@ -29,3 +29,38 @@ def generate(keywords,temp,top_p):
     return result
 
 
+def display():
+    st.write('# Using AI to Generate Sentences from Keywords')
+    st.sidebar.markdown(
+        '''
+        ## This is a demo of a text to text generation model trained with T5 to generate Sentences from Keywords
+
+        *For additional questions and inquiries, please contact **Gagan Bhatia** via [LinkedIn](
+        https://www.linkedin.com/in/gbhatia30/) or [Github](https://github.com/gagan3012).*
+        ''')
+    st.sidebar.write('## Options:')
+
+    keys = st.sidebar.slider(label='Number of keywords', min_value=1, max_value=15, value=3, step=1)
+    top_p = st.sidebar.slider(label='Top k', min_value=0.0, max_value=40.0, value=1.0, step=1.0)
+    temp = st.sidebar.slider(label='Temperature', min_value=0.1, max_value=1.0, value=1.0, step=0.05)
+    st.sidebar.markdown(
+        '''
+        `Number of Keywords:` number of keywords given\n
+        `Temperature:` Float value controlling randomness in boltzmann distribution. Lower temperature results in less random completions. As the temperature approaches zero, the model will become deterministic and repetitive. Higher temperature results in more random completions.\n
+        `Top k:` Integer value controlling diversity. 1 means only 1 word is considered for each step (token), resulting in deterministic completions, while 40 means 40 words are considered at each step. 0 (default) is a special setting meaning no restrictions. 40 generally is a good value.
+        ''')
+
+    st.write('## Enter the keywords:')
+    keywords = []
+    for i in range(1, keys + 1):
+        k = st.text_input('Keyword no.{}'.format(i), value='', key=i)
+        keywords.append(k)
+    keywords = st.multiselect('Review Keywords', keywords, keywords)
+
+    if st.button("Get Answer"):
+        text = generate(keywords, temp, top_p)
+        st.header(text)
+
+
+if __name__ == '__main__':
+    display()
