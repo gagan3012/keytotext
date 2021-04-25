ad6d845dbb00bfad323e478f9424e310c2869a61
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-04-25 03:35:14 +0300

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -31,4 +31,41 @@ class K2TPipeline:
             "no_repeat_ngram_size": 3,
             "early_stopping": True,
         }
-        def __call__(self, ):
+
+        def __call__(self, keywords, **kwargs):
+            inputs = _prepare_inputs_for_k2t(keywords)
+            if not kwargs:
+                kwargs = self.default_generate_kwargs
+
+        def _prepare_inputs_for_k2t(self, keywords):
+            text = str(keywords)
+            text = text.replace(',', '|')
+            text = text.replace("'", "")
+            text = text.replace('[', '')
+            text = text.replace(']', '')
+            texts = text.split(".")
+
+            for txt in texts:
+                if self.model_type == "t5":
+                    txt = txt + " </s>"
+                inputs = self._tokenize(txt, padding=False)
+            return inputs
+
+        def _tokenize(
+                self,
+                inputs,
+                padding=True,
+                truncation=True,
+                add_special_tokens=True,
+                max_length=1024
+        ):
+            inputs = self.tokenizer.batch_encode_plus(
+                inputs,
+                max_length=max_length,
+                add_special_tokens=add_special_tokens,
+                truncation=truncation,
+                padding="max_length" if padding else False,
+                pad_to_max_length=padding,
+                return_tensors="pt"
+            )
+            return inputs
