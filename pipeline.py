4db0644aaea4122eb7df56a9f399fd2230e6518b
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-04-25 04:05:18 +0300

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -1,3 +1,5 @@
+import re
+
 import torch
 from transformers import (
     AutoModelForSeq2SeqLM,
@@ -34,8 +36,17 @@ class K2TPipeline:
 
     def __call__(self, keywords, **kwargs):
         inputs = self._prepare_inputs_for_k2t(keywords)
+        result = ""
         if not kwargs:
             kwargs = self.default_generate_kwargs
+
+        for txt in inputs:
+            input_ids = self._tokenize("{} </s>".format(txt), padding=False)
+            outputs = self.model.generate(input_ids.to(self.device), **kwargs)
+            result += self.tokenizer.decode(outputs[0])
+
+        result = re.sub('<pad>|</s>', "", result)
+        return result
 
     def _prepare_inputs_for_k2t(self, keywords):
         text = str(keywords)
@@ -54,7 +65,7 @@ class K2TPipeline:
             add_special_tokens=True,
             max_length=1024
     ):
-        inputs = self.tokenizer.batch_encode_plus(
+        inputs = self.tokenizer.encode(
             inputs,
             max_length=max_length,
             add_special_tokens=add_special_tokens,
