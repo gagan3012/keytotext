8fc58e2e8d7035249ac3a8e730e62dcf4cee4cbb
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-04-25 03:05:43 +0300

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -1,15 +1,20 @@
-from transformers import(
+import torch
+from transformers import (
     AutoModelForSeq2SeqLM,
     AutoTokenizer,
     PreTrainedModel,
     PreTrainedTokenizer,
 )
 
+
 class K2TPipeline:
     def __init__(
-        self,
-        model : PreTrainedModel,
-        tokenizer : PreTrainedTokenizer,
-        use_cuda: bool
+            self,
+            model: PreTrainedModel,
+            tokenizer: PreTrainedTokenizer,
+            use_cuda: bool
     ):
-
+        self.model = model
+        self.tokenizer = tokenizer
+        self.device = "cuda" if torch.cuda.is_available() and use_cuda else "cpu"
+        self.model.to(self.device)
