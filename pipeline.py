4cfba5616b5901298c5491b94f5269d987e705ec
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-04-25 03:38:00 +0300

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -32,40 +32,36 @@ class K2TPipeline:
             "early_stopping": True,
         }
 
-        def __call__(self, keywords, **kwargs):
-            inputs = _prepare_inputs_for_k2t(keywords)
-            if not kwargs:
-                kwargs = self.default_generate_kwargs
+    def __call__(self, keywords, **kwargs):
+        inputs = self._prepare_inputs_for_k2t(keywords)
+        if not kwargs:
+            kwargs = self.default_generate_kwargs
+        
 
-        def _prepare_inputs_for_k2t(self, keywords):
-            text = str(keywords)
-            text = text.replace(',', '|')
-            text = text.replace("'", "")
-            text = text.replace('[', '')
-            text = text.replace(']', '')
-            texts = text.split(".")
+    def _prepare_inputs_for_k2t(self, keywords):
+        text = str(keywords)
+        text = text.replace(',', '|')
+        text = text.replace("'", "")
+        text = text.replace('[', '')
+        text = text.replace(']', '')
+        texts = text.split(".")
+        return texts
 
-            for txt in texts:
-                if self.model_type == "t5":
-                    txt = txt + " </s>"
-                inputs = self._tokenize(txt, padding=False)
-            return inputs
-
-        def _tokenize(
-                self,
-                inputs,
-                padding=True,
-                truncation=True,
-                add_special_tokens=True,
-                max_length=1024
-        ):
-            inputs = self.tokenizer.batch_encode_plus(
-                inputs,
-                max_length=max_length,
-                add_special_tokens=add_special_tokens,
-                truncation=truncation,
-                padding="max_length" if padding else False,
-                pad_to_max_length=padding,
-                return_tensors="pt"
-            )
-            return inputs
+    def _tokenize(
+            self,
+            inputs,
+            padding=True,
+            truncation=True,
+            add_special_tokens=True,
+            max_length=1024
+    ):
+        inputs = self.tokenizer.batch_encode_plus(
+            inputs,
+            max_length=max_length,
+            add_special_tokens=add_special_tokens,
+            truncation=truncation,
+            padding="max_length" if padding else False,
+            pad_to_max_length=padding,
+            return_tensors="pt"
+        )
+        return inputs
