b603b0969521a87bf03e74cc4b2382c4527c8354
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-04-27 17:44:22 +0300

    Update app.py

--- UI/app.py
+++ UI/app.py
@@ -1,11 +1,13 @@
 import streamlit as st
 from streamlit_tags import st_tags
 from keytotext import pipeline
+import os
+
+os.environ['TOKENIZERS_PARALLELISM'] = 'false'
+
 
 st.set_page_config(
     page_title="Text Generation Using Keywords",
-    #layout="wide",
-    #initial_sidebar_state="expanded",
 )
 
 
