27e4b5bbc78eb724214902d115e37bf648bc762c
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-04-30 16:04:05 +0300

    Update eval.py

--- keytotext/eval.py
+++ keytotext/eval.py
@@ -1,9 +1,15 @@
 import re
 from typing import Optional, Union
 import torch
+from dataclasses import dataclass, field
 from transformers import (
     AutoModelForSeq2SeqLM,
     AutoTokenizer,
-    PreTrainedModel,
-    PreTrainedTokenizer,
+    HfArgumentParser
 )
+
+device = 'cuda' if torch.cuda.is_available else 'cpu'
+
+@dataclass
+class EvalArgs:
+    
