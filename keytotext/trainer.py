57e4d8d1053ba3ba657761b43e6dd0e29062679b
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-05-02 18:09:46 -0400

    Update trainer.py

--- keytotext/trainer.py
+++ keytotext/trainer.py
@@ -18,29 +18,36 @@ for dir in dirc:
     file = glob.glob("{}".format(dir), recursive=True)
     files.append(file)
 
-triple_re=re.compile('(\d)triples')
-data_dct={}
+triple_re = re.compile('(\d)triples')
+data_dct = {}
 for file in files:
     tree = ET.parse(file)
     root = tree.getroot()
-    triples_num=int(triple_re.findall(file)[0])
+    triples_num = int(triple_re.findall(file)[0])
     for sub_root in root:
         for ss_root in sub_root:
-            strutured_master=[]
-            unstructured=[]
+            strutured_master = []
+            unstructured = []
             for entry in ss_root:
                 unstructured.append(entry.text)
-                strutured=[triple.text for triple in entry]
+                strutured = [triple.text for triple in entry]
                 strutured_master.extend(strutured)
-            unstructured=[i for i in unstructured if i.replace('\n','').strip()!='' ]
-            strutured_master=strutured_master[-triples_num:]
-            strutured_master_str=(' && ').join(strutured_master)
-            data_dct[strutured_master_str]=unstructured
+            unstructured = [i for i in unstructured if i.replace('\n', '').strip() != '']
+            strutured_master = strutured_master[-triples_num:]
+            strutured_master_str = (' && ').join(strutured_master)
+            data_dct[strutured_master_str] = unstructured
     print(file)
-mdata_dct={"prefix":[], "input_text":[], "target_text":[]}
-for st,unst in data_dct.items():
+mdata_dct = {"prefix": [], "input_text": [], "target_text": []}
+for st, unst in data_dct.items():
     for i in unst:
         mdata_dct['prefix'].append('webNLG')
         mdata_dct['input_text'].append(st)
         mdata_dct['target_text'].append(i)
 
+df1 = pd.DataFrame(mdata_dct)
+
+train_df = pd.read_csv('NLGDataset.csv', index_col=[0])
+train_df = train_df.iloc[:73424, :]
+train_df = train_df.sample(frac=1)
+batch_size = 8
+num_of_batches = int(len(train_df) / batch_size)
