bb36ac0218ef2d1dc52e291e65af4043875ccaee
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-05-02 18:13:19 -0400

    Update trainer.py

--- keytotext/trainer.py
+++ keytotext/trainer.py
@@ -85,3 +85,55 @@ optimizer = Adafactor(model.parameters(),
                       scale_parameter=False,
                       warmup_init=False)
 
+def trainer(num_of_epochs):
+    model.train()
+    #num_of_epochs = 3
+
+    loss_per_10_steps = []
+    for epoch in range(1, num_of_epochs + 1):
+        print('Running epoch: {}'.format(epoch))
+
+        running_loss = 0
+
+        out = display(progress(1, num_of_batches + 1), display_id=True)
+        evaluation_start = time.time()
+
+        for i in range(num_of_batches):
+            inputbatch = []
+            labelbatch = []
+            new_df = train_df[i * batch_size:i * batch_size + batch_size]
+            for indx, row in new_df.iterrows():
+                input = row['input_text'] + '</s>'
+                labels = row['target_text'] + '</s>'
+                inputbatch.append(input)
+                labelbatch.append(labels)
+            inputbatch = tokenizer.batch_encode_plus(inputbatch, padding=True, max_length=400, return_tensors='pt')[
+                "input_ids"]
+            labelbatch = tokenizer.batch_encode_plus(labelbatch, padding=True, max_length=400, return_tensors="pt")[
+                "input_ids"]
+            inputbatch = inputbatch.to(dev)
+            labelbatch = labelbatch.to(dev)
+
+            # clear out the gradients of all Variables
+            optimizer.zero_grad()
+
+            # Forward propogation
+            outputs = model(input_ids=inputbatch, labels=labelbatch)
+            loss = outputs.loss
+            loss_num = loss.item()
+            logits = outputs.logits
+            running_loss += loss_num
+            if i % 10 == 0:
+                loss_per_10_steps.append(loss_num)
+            out.update(progress(loss_num, i, num_of_batches + 1))
+
+            # calculating the gradients
+            loss.backward()
+
+            # updating the params
+            optimizer.step()
+
+        running_loss = running_loss / int(num_of_batches)
+        evaluation_total = time.time() - evaluation_start
+        print('Training time:', evaluation_total)
+        print('Epoch: {} , Running loss: {}'.format(epoch, running_loss))
