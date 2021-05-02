3a5d3b1e5df8f88c8c12bc283260467020f03c11
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-05-02 18:40:22 -0400

    Update trainer.py

--- keytotext/trainer.py
+++ keytotext/trainer.py
@@ -1,11 +1,8 @@
 import pandas as pd
 import torch
-import os
 from transformers import T5Tokenizer, T5ForConditionalGeneration, Adafactor
 import time
-import sentencepiece
 import glob
-import os
 import re
 import xml.etree.ElementTree as ET
 from IPython.display import HTML, display
