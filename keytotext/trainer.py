0fc66bdab6a5994e6c8c9503cf16af74f947bcf1
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-05-02 18:12:15 -0400

    Update trainer.py

--- keytotext/trainer.py
+++ keytotext/trainer.py
@@ -67,3 +67,20 @@ def progress(loss, value, max=100):
 value='{value}'max='{max}',style='width: 100%'>{value}
       </progress>
              """.format(loss=loss, value=value, max=max))
+
+tokenizer = T5Tokenizer.from_pretrained('t5-base')
+model = T5ForConditionalGeneration.from_pretrained('t5-base',
+                                             return_dict=True)
+#moving the model to GPU
+model.to(dev)
+
+optimizer = Adafactor(model.parameters(),lr=1e-3,
+                      eps=(1e-30, 1e-3),
+                      clip_threshold=1.0,
+                      decay_rate=-0.8,
+                      beta1=None,
+                      weight_decay=0.0,
+                      relative_step=False,
+                      scale_parameter=False,
+                      warmup_init=False)
+
