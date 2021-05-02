de9fea7afe325be9ea547ccc0952c56b75929819
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-05-02 18:02:16 -0400

    Update eval.py

--- keytotext/eval.py
+++ keytotext/eval.py
@@ -158,5 +158,12 @@ def main():
 
     test = pd.read_csv('data/TestNLG.csv')
     keywords_test = test['input_text']
+
+    nlp = eval_pipeline("k2t")
+    prediction = []
     for key in keywords_test:
-        
+        prediction.append(nlp(keywords=key))
+
+    test['predctions'] = prediction
+
+
