d96635a3c41a77f0d889e75ac7b79c14435d4f91
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-05-01 22:33:08 -0400

    Update eval.py

--- keytotext/eval.py
+++ keytotext/eval.py
@@ -24,8 +24,17 @@ class EvalArgs:
     )
 
 def eval():
-    
+    result = ""
+    if not kwargs:
+        kwargs = self.default_generate_kwargs
 
+    for txt in inputs:
+        input_ids = self._tokenize("{} </s>".format(txt), padding=False)
+        outputs = self.model.generate(input_ids.to(self.device), **kwargs)
+        result += self.tokenizer.decode(outputs[0])
+
+    result = re.sub("<pad>|</s>", "", result)
+    return result.strip()
 
 
 
