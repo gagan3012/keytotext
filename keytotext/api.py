cda77f69bfa5d43efeaa921bfbcadb7d44a50374
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-05-03 09:38:35 -0400

    Update api.py

--- keytotext/api.py
+++ keytotext/api.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI, Depends
 from pydantic import BaseModel
+from typing import List
+from starlette.testclient import TestClient
 
 app = FastAPI()
+
 
 @app.post("/")
 def k2tapi(data: List[str]):
     print(data)
+
