52d941978cb8e3eaed31753c16b211343283df59
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-05-03 17:17:17 -0400

    Update api.py

--- keytotext/api.py
+++ keytotext/api.py
@@ -4,15 +4,18 @@ from keytotext.pipeline import pipeline
 
 app = FastAPI()
 
+
 def generate(keywords, model="k2t"):
     nlp = pipeline(model)
     return nlp(keywords)
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.post("/api/")
+
+@app.post("/api")
 def k2t_post(data: List[str]):
     return {
         "keywords": data,
