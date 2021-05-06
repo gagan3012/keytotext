9fbcb1bc3dee1608d82fd176e74d4dff5df6441d
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-05-05 19:50:51 -0700

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -2,7 +2,7 @@ from keytotext.pipeline import pipeline
 import requests
 
 
-def tests():
+def asastests():
     r = requests.get('http://127.0.0.1:5000')
     if r.status_code == 404:
         print("error")
