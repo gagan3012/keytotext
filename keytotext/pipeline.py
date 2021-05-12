e25d10dbf72cc2d4ac34ad22fdbff48bbc51722b
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-05-12 11:49:47 -0700

    Update pipeline.py

--- keytotext/pipeline.py
+++ keytotext/pipeline.py
@@ -98,3 +98,4 @@ def pipeline(
         return task_class(model=model, tokenizer=tokenizer, use_cuda=use_cuda)
     if task == "mrm8488/t5-base-finetuned-common_gen":
         return task_class(model=model, tokenizer=tokenizer, use_cuda=use_cuda)
+
