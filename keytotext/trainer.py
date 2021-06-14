0fad617537d4088aebea23ba05552a7ff5cfb1a9
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-06-14 15:45:55 -0700

    Update trainer.py

--- keytotext/trainer.py
+++ keytotext/trainer.py
@@ -106,8 +106,7 @@ class PLDataModule(LightningDataModule):
         :param split:
         """
         super().__init__()
-        self.test_df = test_df
-        self.train_df = train_df
+        self.data_df = data_df
         self.split = split
         self.batch_size = batch_size
         self.target_max_token_len = target_max_token_len
