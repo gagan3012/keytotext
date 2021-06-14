5e6035b51bd05a161ebc45e1f81c6fd5b2a27099
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-06-14 12:57:35 -0700

    Update trainer.py

--- keytotext/trainer.py
+++ keytotext/trainer.py
@@ -16,5 +16,6 @@ from pytorch_lightning.loggers import wandb
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 
+torch.cuda.empty_cache()
 pl.seed_everything(42)
 
