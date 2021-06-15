e58f28b26fc897bf497f6c9b4a37cd0dc2577085
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-06-15 01:07:46 -0700

    Update __init__.py

--- keytotext/__init__.py
+++ keytotext/__init__.py
@@ -5,6 +5,7 @@ Keytotext converts a list of Keywords to sentences and larger texts
 
 
 __version__ = "0.3.0"
+__version__ = "0.3.1"
 
 from .pipeline import pipeline
 from .trainer import KeytotextTrainer
