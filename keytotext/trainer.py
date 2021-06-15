69da39fe74e3216ecba3c5fc0943fb34cbe5ffbb
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-06-15 00:08:11 -0700

    Update trainer.py

--- keytotext/trainer.py
+++ keytotext/trainer.py
@@ -229,7 +229,6 @@ class LightningModel(pl.LightningModule):
         """ configure optimizers """
         return AdamW(self.parameters(), lr=0.0001)
 
-        avg_traning_loss = np.round(
             torch.mean(torch.stack([x["loss"] for x in training_step_outputs])).item(),
             4,
         )
