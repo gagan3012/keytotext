88d363b1736d89ea4b7cb5ac80a2f30d5ce808b4
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-06-17 18:55:10 -0700

    Update trainer.py

--- keytotext/trainer.py
+++ keytotext/trainer.py
@@ -137,7 +137,6 @@ class PLDataModule(LightningDataModule):
     def test_dataloader(self):
         """ test dataloader """
         return DataLoader(
-            self.test_dataset, batch_size=self.batch_size, shuffle=False, num_workers=2
         )
 
     def val_dataloader(self):
