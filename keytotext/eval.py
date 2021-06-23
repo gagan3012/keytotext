1fc6aa1f25c8e3990f6d0a6daca002d73419381a
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-06-23 10:47:27 -0700

    Delete eval.py

--- keytotext/eval.py
+++ keytotext/eval.py
@@ -1,7 +1,3 @@
-    if task == "k2t-base":
-        return task_class(model=model, tokenizer=tokenizer, use_cuda=use_cuda)
-
-
 def eval():
     test = pd.read_csv("data/TestNLG.csv")
     keywords_test = test["input_text"]
