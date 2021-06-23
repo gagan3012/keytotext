68df0265c35fb8f8d6e83de63530b152e3257e98
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-06-23 10:44:25 -0700

    Delete eval.py

--- keytotext/eval.py
+++ keytotext/eval.py
@@ -1,4 +1,3 @@
-        self.device = "cuda" if torch.cuda.is_available() and use_cuda else "cpu"
         self.model.to(self.device)
 
         assert self.model.__class__.__name__ in ["T5ForConditionalGeneration"]
