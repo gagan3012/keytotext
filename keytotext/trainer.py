5d341d6cbef87fead304c0c4330414aebf4427a0
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-06-30 22:47:51 -0700

    Update trainer.py

--- keytotext/trainer.py
+++ keytotext/trainer.py
@@ -253,6 +253,7 @@ class trainer:
     def train(
             self,
             train_df: pd.DataFrame,
+            test_df: pd.DataFrame,
             source_max_token_len: int = 512,
             target_max_token_len: int = 512,
             batch_size: int = 8,
