7b338cd120147ff0b7675f2ffc0a984bb83c2aa4
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-02 16:48:08 -0400

    Update trainer.py

--- keytotext/trainer.py
+++ keytotext/trainer.py
@@ -212,6 +212,7 @@ class LightningModel(pl.LightningModule):
             decoder_attention_mask=labels_attention_mask,
             labels=labels,
         )
+        val_acc = self.val_acc(outputs.logits.argmax(1), labels)
         self.log("val_loss", loss, prog_bar=True, logger=True)
         self.log(f"val_acc", acc, prog_bar=True,logger=True)
         return loss
