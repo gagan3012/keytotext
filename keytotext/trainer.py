58dd23b7ab54423f625f780fb5be758531f6e0b6
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-02 16:48:14 -0400

    Update trainer.py

--- keytotext/trainer.py
+++ keytotext/trainer.py
@@ -214,7 +214,6 @@ class LightningModel(pl.LightningModule):
         )
         val_acc = self.val_acc(outputs.logits.argmax(1), labels)
         self.log("val_loss", loss, prog_bar=True, logger=True)
-        self.log(f"val_acc", acc, prog_bar=True,logger=True)
         return loss
 
     def test_step(self, batch, batch_size):
