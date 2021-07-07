f4a247614d6bd946cd58b2552dd24827526b0130
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-07 16:24:24 -0400

    Update main.py

--- trainer/main.py
+++ trainer/main.py
@@ -2,3 +2,4 @@ from keytotext import trainer
 
 model = trainer()
 model.from_pretrained(model_name="t5-small")
+model.train(train_df = train_df[:2000],test_df=test_df,batch_size=4, max_epochs=3, use_gpu=True)
