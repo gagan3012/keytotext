cd8f4ea5df1d1a0d667131fbd7f6da2d34c363e7
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-07 16:24:28 -0400

    Update main.py

--- trainer/main.py
+++ trainer/main.py
@@ -3,3 +3,4 @@ from keytotext import trainer
 model = trainer()
 model.from_pretrained(model_name="t5-small")
 model.train(train_df = train_df[:2000],test_df=test_df,batch_size=4, max_epochs=3, use_gpu=True)
+model.upload("gagan3012","k2t-test3")
