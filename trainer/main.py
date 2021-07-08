a177e7155b2c51009e5b9ec4dac113a95548b223
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-08 00:50:16 -0400

    Update main.py

--- trainer/main.py
+++ trainer/main.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 dataset = load_dataset('common_gen',split='train')
+df['keywords']=dataset['concepts']
 model = trainer()
 model.from_pretrained(model_name="t5-small")
 model.train(train_df = train_df,test_df=test_df,batch_size=4, max_epochs=3, use_gpu=True)
