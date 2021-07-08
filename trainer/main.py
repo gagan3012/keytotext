aeebb3e269a4e41513820334feabd73d983e6dc7
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-08 00:55:13 -0400

    Update main.py

--- trainer/main.py
+++ trainer/main.py
@@ -13,6 +13,7 @@ def clean(keywords):
 df['keywords'] = df['keywords'].apply(lambda x: clean(x))
 train_df,test_df = train_test_split(df,test_size=0.01, random_state=42)
 
+
 model = trainer()
 model.from_pretrained(model_name="t5-small")
 model.train(train_df = train_df,test_df=test_df,batch_size=4, max_epochs=3, use_gpu=True)
