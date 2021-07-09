e923d7fbe039bdd44920559d74bcb3e483f4a9a3
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-09 11:16:21 -0400

    Update main.py

--- trainer/main.py
+++ trainer/main.py
@@ -1,6 +1,5 @@
 from keytotext import trainer, make_dataset
 
-train_df, test_df = train_test_split(df, test_size=0.01, random_state=42)
 
 model = trainer()
 model.from_pretrained(model_name="t5-small")
