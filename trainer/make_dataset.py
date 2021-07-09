2f2e60f97e604cd4cf8b2544bda9f8eed9d74119
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-09 11:04:52 -0400

    Delete make_dataset.py

--- trainer/make_dataset.py
+++ trainer/make_dataset.py
@@ -1,4 +1,3 @@
-    df['text'] = dataset['target']
     df['keywords'] = df['keywords'].apply(lambda x: clean(x))
     train_df, test_df = train_test_split(df, test_size=0.01, random_state=42)
     return train_df, test_df
