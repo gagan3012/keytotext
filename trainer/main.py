61d99b1db8d667b566fc540c8f420988a72dc42e
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-09 11:10:43 -0400

    Update main.py

--- trainer/main.py
+++ trainer/main.py
@@ -12,7 +12,6 @@ def create_df(dataset):
     train_df, test_df = train_test_split(df, test_size=0.01, random_state=42)
     return train_df, test_df
 
-
 model = trainer()
 model.from_pretrained(model_name="t5-small")
 model.train(train_df=train_df, test_df=test_df, batch_size=4, max_epochs=3, use_gpu=True)
