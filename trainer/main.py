b1b07b0760ac5b03e6fe897d0084bd5daebc5735
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-09 15:36:37 -0400

    Update main.py

--- trainer/main.py
+++ trainer/main.py
@@ -1,7 +1,6 @@
 from keytotext import trainer, make_dataset
 
 train_df = make_dataset(split='train')
-test_df = make_dataset(split='test')
 test_df = make_dataset('common_gen', split='test')
 
 model = trainer()
