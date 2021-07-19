de80fbc005173c8bfbbac65d517c77ae42b9bdde
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-19 12:44:37 -0700

    Update trainer.py

--- keytotext/trainer.py
+++ keytotext/trainer.py
@@ -473,6 +473,7 @@ class trainer:
                 'Rouge_2 Mid Precision': results["rouge2"].mid.precision,
                 'Rouge_2 Mid recall': results["rouge2"].mid.recall,
                 'Rouge_2 Mid F1': results["rouge2"].mid.fmeasure,
+                'Rouge_2 High Precision': results["rouge2"].high.precision,
     def upload(self, hf_username, model_name):
         hf_password = getpass("Enter your HuggingFace password")
         if Path('./model').exists():
