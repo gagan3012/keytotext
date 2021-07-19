576633a52e72d44036d4fd8dde419937b155dddb
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-07-19 12:36:19 -0700

    Update trainer.py

--- keytotext/trainer.py
+++ keytotext/trainer.py
@@ -440,6 +440,7 @@ class trainer:
             self,
             test_df: pd.DataFrame,
             metrics: str = "rouge"
+    ):
     def upload(self, hf_username, model_name):
         hf_password = getpass("Enter your HuggingFace password")
         if Path('./model').exists():
