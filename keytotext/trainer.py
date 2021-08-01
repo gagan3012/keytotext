a0c538bca063b5520a4011c3d2f42bead187a908
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-08-01 10:24:28 -0700

    Update trainer.py

--- keytotext/trainer.py
+++ keytotext/trainer.py
@@ -496,8 +496,8 @@ class trainer:
 
     def upload(self, hf_username, model_name):
         hf_password = getpass("Enter your HuggingFace password")
-        if Path('./model').exists():
-            shutil.rmtree('./model')
+        if Path("./model").exists():
+            shutil.rmtree("./model")
         token = HfApi().login(username=hf_username, password=hf_password)
         del hf_password
         model_url = HfApi().create_repo(token=token, name=model_name, exist_ok=True)
