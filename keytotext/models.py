adc8c9f80aa68f831cab744c7888eaaa2f9a5e04
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-08-01 10:17:51 -0700

    Update models.py

--- keytotext/models.py
+++ keytotext/models.py
@@ -117,7 +117,6 @@ class K2TPipeline:
         return texts
 
     def _tokenize(
-            add_special_tokens=True,
             max_length=1024,
     ):
         inputs = self.tokenizer.encode(
