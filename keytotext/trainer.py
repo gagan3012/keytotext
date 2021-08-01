5ce69d66b51b3c5dff1ced24acf5acbfe4235339
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-08-01 10:19:03 -0700

    Update trainer.py

--- keytotext/trainer.py
+++ keytotext/trainer.py
@@ -30,7 +30,6 @@ class DataModule(Dataset):
     """
 
     def __init__(
-            self,
             data: pd.DataFrame,
             tokenizer: T5Tokenizer,
             source_max_token_len: int = 512,
