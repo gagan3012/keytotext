f192d1313be2a5c65ac0c1bfbf2c0f31b286d1bc
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-09-19 09:28:24 -0700

    Update create_tokenizer.py

--- scripts/create_tokenizer.py
+++ scripts/create_tokenizer.py
@@ -1,3 +1,7 @@
 """SentencePieceUnigramTokenizer and script to train it on a corpus available from Huggingface Datasets."""
 
 import json
+import argparse
+from typing import Iterator, List, Union
+
+import datasets
