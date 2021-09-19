ff4f1be916fbaee9d9e330898fa92b566cfb5591
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-09-19 09:28:55 -0700

    Update create_tokenizer.py

--- scripts/create_tokenizer.py
+++ scripts/create_tokenizer.py
@@ -17,3 +17,5 @@ from tokenizers.processors import TemplateProcessing
 class SentencePieceUnigramTokenizer(BaseTokenizer):
     """
     This class is a copy of `DeDLOC's tokenizer implementation <https://github.com/yandex-research/DeDLOC/blob/main/sahajbert/tokenizer/tokenizer_model.py>`__ .
+    Custom SentencePiece Unigram Tokenizer with NMT, NKFC, spaces and lower-casing characters normalization
+    Represents the Unigram algorithm, with the pretokenization used by SentencePiece
