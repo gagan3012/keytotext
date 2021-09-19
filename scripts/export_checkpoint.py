dcab49dec7c13da361d2e55e0df6a8b9d1140209
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-09-19 09:44:42 -0700

    Update export_checkpoint.py

--- scripts/export_checkpoint.py
+++ scripts/export_checkpoint.py
@@ -1,3 +1,8 @@
 import argparse
 from transformers import T5ForConditionalGeneration, TFT5ForConditionalGeneration
 
+
+def main(args):
+    pt_model = T5ForConditionalGeneration.from_pretrained(args.model_dir, from_flax=True)
+    pt_model.save_pretrained(args.model_dir)
+    tf_model = TFT5ForConditionalGeneration.from_pretrained(args.model_dir, from_pt=True)
