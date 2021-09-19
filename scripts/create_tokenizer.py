c40bde59c81d9101ae602b6527901bd7396cad8e
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-09-19 09:28:44 -0700

    Update create_tokenizer.py

--- scripts/create_tokenizer.py
+++ scripts/create_tokenizer.py
@@ -8,3 +8,6 @@ import datasets
 from datasets.utils import DownloadConfig
 from transformers import T5Config
 
+from tokenizers import AddedToken, Regex, Tokenizer, decoders, normalizers, pre_tokenizers, trainers
+from tokenizers.implementations.base_tokenizer import BaseTokenizer
+from tokenizers.models import Unigram
