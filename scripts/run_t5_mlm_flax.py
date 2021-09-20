8a070c41a6f1df783eb93ac965cd7cf1660020f9
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-09-20 09:57:53 -0700

    Update run_t5_mlm_flax.py

--- scripts/run_t5_mlm_flax.py
+++ scripts/run_t5_mlm_flax.py
@@ -498,7 +498,9 @@ if __name__ == "__main__":
     if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
         # If we pass only one argument to the script and it's the path to a json file,
         # let's parse it to get our arguments.
-        model_args, data_args, training_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[1]))
+        model_args, data_args, training_args = parser.parse_json_file(
+            json_file=os.path.abspath(sys.argv[1])
+        )
     else:
         model_args, data_args, training_args = parser.parse_args_into_dataclasses()
 
