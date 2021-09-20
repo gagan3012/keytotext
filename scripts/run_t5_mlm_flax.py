38538b36f21a544281f06174a389e6fe2f88802b
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-09-20 09:57:50 -0700

    Update run_t5_mlm_flax.py

--- scripts/run_t5_mlm_flax.py
+++ scripts/run_t5_mlm_flax.py
@@ -492,7 +492,9 @@ if __name__ == "__main__":
     # or by passing the --help flag to this script.
     # We now keep distinct sets of args, for a cleaner separation of concerns.
 
-    parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments))
+    parser = HfArgumentParser(
+        (ModelArguments, DataTrainingArguments, TrainingArguments)
+    )
     if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
         # If we pass only one argument to the script and it's the path to a json file,
         # let's parse it to get our arguments.
