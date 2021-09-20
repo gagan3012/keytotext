cbe4b4d6e2ed2719f1e28254771aa551103ddea2
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-09-20 09:55:18 -0700

    Update export_checkpoint.py

--- scripts/export_checkpoint.py
+++ scripts/export_checkpoint.py
@@ -15,4 +15,4 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_dir', type=str, default='.')
+    parser.add_argument("--model_dir", type=str, default=".")
