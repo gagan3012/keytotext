d494f6af2c697adc4f0534d38e84e35bacb06519
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-09-28 10:37:50 -0700

    Update main.py

--- trainer/main.py
+++ trainer/main.py
@@ -6,6 +6,6 @@ test_df = make_dataset('common_gen', split='test')
 
 model = trainer()
 model.from_pretrained(model_name="t5-base")
-model.train(train_df=train_df, test_df=eval_df, batch_size=4, max_epochs=10, use_gpu=True)
+model.train(train_df=train_df, test_df=eval_df, batch_size=4, max_epochs=10, use_gpu=True,tpu_cores=8)
 print(model.evaluate(test_df=test_df))
 model.upload(hf_username="gagan3012", model_name="k2t-base")
