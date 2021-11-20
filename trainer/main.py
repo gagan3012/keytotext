0e67f11e3948b3715632749d581e034d6e09309c
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-11-20 14:00:00 +0100

    Update main.py

--- trainer/main.py
+++ trainer/main.py
@@ -1,3 +1,7 @@
+# for TPU trainign with tf.data.Dataset
+
+import torch
+import torch_xla.core.xla_model as xm
 from keytotext import trainer, make_dataset
 
 train_df = make_dataset('common_gen', split='train')
